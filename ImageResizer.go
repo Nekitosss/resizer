25f8905d2d0ed1e269e71557d4831166d13870a5
Nikita <patskovn@yahoo.com>
2018-01-04 22:47:39 +0500

    Add image resizer

---
+++ ImageResizer.go
@@ -0,0 +1,39 @@
+package resizer
+
+import (
+	"image"
+	"github.com/nfnt/resize"
+)
+
+
+type ResizeRequest struct {
+
+	URL    string `json:"url,omitempty"`
+
+	Height int    `json:"height,omitempty"`
+
+	Width  int    `json:"width,omitempty"`
+
+}
+
+
+func Resize(width int, height int, img *image.Image) *image.Image {
+	original := *img
+	originalWidth := original.Bounds().Max.X
+	originalHeight := original.Bounds().Max.Y
+
+	preferredWidth := uint(0)
+	preferredHeight := uint(0)
+
+	if originalWidth < originalHeight {
+		preferredHeight = uint(height)
+		preferredWidth = uint(float32(originalWidth * height) / float32(originalHeight))
+
+	} else {
+		preferredWidth = uint(width)
+		preferredHeight = uint(float32(originalHeight * width) / float32(originalWidth))
+	}
+
+	resizedImage := resize.Resize(preferredWidth, preferredHeight, original, resize.NearestNeighbor)
+	return &resizedImage
+}
